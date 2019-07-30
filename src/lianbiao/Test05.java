5069aa33266e7137bb8d565b56aec065d7b50d88
donghao <donghao@autohome.com.cn>
2019-07-30 16:15:14 +0800

    合并有序的 2个链表 递归

--- src/lianbiao/Test05.java
+++ src/lianbiao/Test05.java
@@ -9,19 +9,27 @@ package lianbiao;
 public class Test05 {
 
     public static void main(String[] args) {
+        //非递归
+        Node mergeHead1 = merge1();
+        printLink(mergeHead1);
+        System.out.println("===============================================================================================================");
+
+        //非递归(不额外创建新节点)
+        Node mergeHead2 = merge2();
+        printLink(mergeHead2);
+        System.out.println("===============================================================================================================");
+
+
+        //递归
         Node head1 = buildNodeList1();
         printLink(head1);
-
         Node head2 = buildNodeList2();
         printLink(head2);
 
-        //非递归
-        Node head = merge(head1, head2);
-        printLink(head);
-
-        //递归
-        Node recursionHead = recursionMerge(head1, head2);
+        Node recursionHead = recursionMerge(head1,head2);
         printLink(recursionHead);
+        System.out.println("===============================================================================================================");
+
     }
 
     private static Node recursionMerge(Node head1, Node head2) {
@@ -46,7 +54,13 @@ public class Test05 {
         return newHead;
     }
 
-    private static Node merge(Node head1, Node head2) {
+    private static Node merge1() {
+        Node head1 = buildNodeList1();
+        printLink(head1);
+
+        Node head2 = buildNodeList2();
+        printLink(head2);
+
         if (head1 == null && head2 == null) {
             return null;
         }
@@ -59,17 +73,9 @@ public class Test05 {
 
         Node p1 = head1;
         Node p2 = head2;
-        Node head = null;
-        Node tail = null;
-        //新链表的头指针
-        if (p1.data <= p2.data) {
-            head = new Node(p1.data);
-            p1 = p1.next;
-        } else {
-            head = new Node(p2.data);
-            p2 = p2.next;
-        }
-        tail = head;
+        //哨兵
+        Node guard = new Node(-1);
+        Node tail = guard;
 
         Node temp = null;
         while (p1 != null && p2 != null) {
@@ -91,7 +97,51 @@ public class Test05 {
             tail.next = p1;
         }
 
-        return head;
+        return guard.next;
+    }
+
+    private static Node merge2() {
+        Node head1 = buildNodeList1();
+        printLink(head1);
+
+        Node head2 = buildNodeList2();
+        printLink(head2);
+
+        if (head1 == null && head2 == null) {
+            return null;
+        }
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+
+        Node p1 = head1;
+        Node p2 = head2;
+        //哨兵
+        Node guard = new Node(-1);
+        Node tail = guard;
+        while (p1 != null && p2 != null) {
+            if (p1.data <= p2.data) {
+                 tail.next = p1;
+                 tail = p1;
+                 p1 = p1.next;
+            } else {
+                tail.next = p2;
+                tail = p2;
+                p2 = p2.next;
+            }
+        }
+
+        if (p1 == null) {
+            tail.next = p2;
+        }
+        if (p2 == null) {
+            tail.next = p1;
+        }
+
+        return guard.next;
     }
 
     private static void printLink(Node head) {
