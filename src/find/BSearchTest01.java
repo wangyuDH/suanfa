44afa990dcab3a1c9c1336c1ee06f573de545dbe
donghao <donghao@autohome.com.cn>
2019-09-17 09:35:54 +0800

    折半查找

--- src/find/BSearchTest01.java
+++ src/find/BSearchTest01.java
@@ -2,28 +2,28 @@ package find;
 
 /**
  * 折半查找-基础版
- *   注意点：
- *    1 退出条件
- *    2 mid的取值
- *    3 low和hign的更新
+ * 注意点：
+ * 1 退出条件
+ * 2 mid的取值
+ * 3 low和hign的更新
  */
 public class BSearchTest01 {
     public static final int key = 10;
 
     public static void main(String[] args) {
         int array[] = {2, 5, 7, 8, 10, 33, 57};
-
-        int index1 = findKey(array, key);
-        System.out.println("index1: " + index1);
-
         int low = 0;
         int hign = array.length - 1;
+
+        int index1 = findKey(array, low, hign, key);
+        System.out.println("index1: " + index1);
+
         int index2 = diguiFindKey(array, low, hign, key);
         System.out.println("index2: " + index2);
     }
 
     private static int diguiFindKey(int[] a, int low, int hign, int key) {
-        if(low > hign){
+        if (low > hign) {
             return -1;
         }
 
@@ -37,10 +37,7 @@ public class BSearchTest01 {
         }
     }
 
-    private static int findKey(int[] a, int key) {
-        int low = 0;
-        int hign = a.length - 1;
-
+    private static int findKey(int[] a, int low, int hign, int key) {
         while (hign >= low) {
             int mid = low + (hign - low) / 2;
             if (a[mid] == key) {
